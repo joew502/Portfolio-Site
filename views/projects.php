6666decb4f1b1cb76e8ac36fc26367db32a1c048
Joe Wilson <joewilson502@gmail.com>
2024-11-28 14:03:30 +0000

    Initial Front End

--- views/projects.php
+++ views/projects.php
@@ -1,4 +1,16 @@
 <?php require 'partials/header.php'; ?>
-    <h1>Joe Wilson - Projects</h1>
-    <?php require 'partials/nav.php'; ?>
+    <div class="min-h-full">
+        <?php require 'partials/nav.php'; ?>
+        <header class="bg-white shadow">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Projects</h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <!-- Your content -->
+                <h1>Hello World</h1>
+            </div>
+        </main>
+    </div>
 <?php require 'partials/footer.php'; ?>

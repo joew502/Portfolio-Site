151fe15115dddf00c9a67caca1aca13738cac356
Joe Wilson <joewilson502@gmail.com>
2025-01-22 16:04:10 +0000

    Initial Projects Mock Up

--- views/projects.php
+++ views/projects.php
@@ -7,9 +7,72 @@
             </div>
         </header>
         <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <!-- Your content -->
-                <h1>Hello World</h1>
+            <div class="mx-auto max-w-4xl px-4 py-6 sm:px-6 lg:px-8">
+                <div class="w-full block justify-center">
+                    <div class="w-full flex justify-center border rounded-md border-[3px] border-[#ff690D] p-3">
+                        <div class="size-[160px] shrink-0 overflow-hidden rounded-md border border-gray-200">
+                            <img src="/assets/website-icon.png" alt="Website Icon" class="size-full object-cover">
+                        </div>
+
+                        <div class="ml-4 flex flex-1 flex-col">
+                            <div>
+                                <div class="flex justify-between text-base font-medium text-black">
+                                    <div class="text-xl font-bold">
+                                        Personal Website
+                                    </div>
+                                    <a href="https://github.com/joew502" class="flex align-baseline ml-4">
+                                        <svg class="mt-0.3 w-7 h-7 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div class="flex items-baseline space-x-2 mt-1 text-white">
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2">PHP</div>
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2">Tailwind CSS</div>
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2">JavaScript</div>
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2">Self-Hosted</div>
+                                </div>
+                                <p class="mt-1 text-base text-black">A personal project to display my skills, projects and services. Created in PHP with JavaScript and Tailwind CSS. Self hosted on Apache on an Debian Linux Server via a Cloudflare Zero Trust Tunnel.</p>
+                            </div>
+                            <!--
+                            <div class="flex flex-1 items-end justify-between text-sm">
+                                <p class="text-gray-500">Qty 1</p>
+
+                                <div class="flex">
+                                    <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                                </div>
+                            </div>-->
+                        </div>
+                    </div>
+                    <div class="w-full flex justify-center border rounded-md border-[3px] border-[#ff690D] p-3 mt-5">
+                        <div class="size-[160px] shrink-0 overflow-hidden rounded-md border border-gray-200">
+                            <img src="/assets/FMS-icon.png" alt="Website Icon" class="size-full object-cover">
+                        </div>
+
+                        <div class="ml-4 flex flex-1 flex-col">
+                            <div>
+                                <div class="flex justify-between text-base font-medium text-black">
+                                    <div class="text-xl font-bold">
+                                        Financial Management Software
+                                    </div>
+                                    <a href="https://github.com/joew502" class="flex align-baseline ml-4">
+                                        <svg class="mt-0.3 w-7 h-7 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div class="flex items-baseline space-x-2 mt-1 text-white">
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2">Java</div>
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2">Swing</div>
+                                </div>
+                                <p class="mt-1 text-base text-black">Financial Management Software for Student Clubs and Societies. I created this project qs part of my final year dissertation.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full flex justify-center border rounded-md border-[3px] border-[#ff690D] p-3 mt-5">
+                        Hello World
+                    </div>
+                </div>
             </div>
         </main>
     </div>

e3d7ff4665ed6f425758dc01e5dc52b2bb4ca548
Joe Wilson <joewilson502@gmail.com>
2025-02-27 12:05:41 +0000

    Added projects

--- views/projects.php
+++ views/projects.php
@@ -9,7 +9,7 @@
         <main>
             <div class="mx-auto max-w-lg sm:max-w-4xl px-4 py-6 sm:px-6 lg:px-8">
                 <div class="w-full block justify-center">
-                    <div class="w-full block sm:flex justify-center border rounded-md border-[3px] border-[#ff690D] p-3">
+                    <div class="w-full block sm:flex justify-center border rounded-md border-[3px] border-black p-3">
                         <div class="size-[160px] mx-auto shrink-0 overflow-hidden rounded-md border border-gray-200">
                             <img src="/assets/website-icon.png" alt="Website Icon" class="size-full object-cover">
                         </div>
@@ -20,7 +20,7 @@
                                     <div class="text-xl font-bold">
                                         Personal Website
                                     </div>
-                                    <a href="https://github.com/joew502" class="flex align-baseline ml-4">
+                                    <a href="https://github.com/joew502/Portfolio-Site" class="flex align-baseline ml-4">
                                         <svg class="mt-0.3 w-7 h-7 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                             <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"/>
                                         </svg>
@@ -44,9 +44,36 @@
                             </div>-->
                         </div>
                     </div>
-                    <div class="w-full block sm:flex justify-center border rounded-md border-[3px] border-[#ff690D] p-3 mt-5">
+                    <div class="w-full block sm:flex justify-center border rounded-md border-[3px] border-black p-3 mt-5">
                         <div class="size-[160px] mx-auto shrink-0 overflow-hidden rounded-md border border-gray-200">
-                            <img src="/assets/FMS-icon.png" alt="Website Icon" class="size-full object-cover">
+                            <img src="/assets/Forum-icon.png" alt="Forum Icon" class="size-full object-cover">
+                        </div>
+
+                        <div class="sm:ml-4 flex flex-1 flex-col">
+                            <div>
+                                <div class="flex justify-between text-base font-medium text-black">
+                                    <div class="text-xl font-bold">
+                                        University Chat Forum
+                                    </div>
+                                    <a href="https://github.com/joew502/University-Chat-Forum" class="flex align-baseline ml-4">
+                                        <svg class="mt-0.3 w-7 h-7 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div class="flex items-baseline space-x-2 mt-1 text-white overflow-x-auto">
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">PHP</div>
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">JavaScript</div>
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">SQL</div>
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">CSS</div>
+                                </div>
+                                <p class="mt-1 text-base text-black">Developed with a group as part of a University Coursework, this is a Chat Forum that combines informal communication between students with official module and society Announcements and discussions. We used the Kanban software development methodology to create a publicly hosted website built with PHP and JavaScript connected to a MySQL Database. The site was designed to be the singular chat forum and announcement page for university students with three main sections for Academic, Societies and Community. The site is capable of having individual user accounts, each with associated levels, privileges and subscribed clubs. Each post can have comments and multiple layers of sub-comments, all with respective up and down voting capability.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full block sm:flex justify-center border rounded-md border-[3px] border-black p-3 mt-5">
+                        <div class="size-[160px] mx-auto shrink-0 overflow-hidden rounded-md border border-gray-200">
+                            <img src="/assets/FMS-icon.png" alt="FMS Icon" class="size-full object-cover">
                         </div>
 
                         <div class="sm:ml-4 flex flex-1 flex-col">
@@ -55,7 +82,7 @@
                                     <div class="text-xl font-bold">
                                         Financial Management Software
                                     </div>
-                                    <a href="https://github.com/joew502" class="flex align-baseline ml-4">
+                                    <a href="https://github.com/joew502/Financial-Management-Software" class="flex align-baseline ml-4">
                                         <svg class="mt-0.3 w-7 h-7 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                             <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"/>
                                         </svg>
@@ -65,12 +92,61 @@
                                     <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">Java</div>
                                     <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">Swing</div>
                                 </div>
-                                <p class="mt-1 text-base text-black">Financial Management Software for Student Clubs and Societies. I created this project as part of my final year dissertation.</p>
+                                <p class="mt-1 text-base text-black">Developed as part of my final year dissertation, this is a free, focused Financial Management Software that is optimised for use by Student Clubs and Societies where current market options are unsuited and overpriced.</p>
                             </div>
                         </div>
                     </div>
-                    <div class="w-full flex justify-center border rounded-md border-[3px] border-[#ff690D] p-3 mt-5">
-                        Hello World
+                    <div class="w-full block sm:flex justify-center border rounded-md border-[3px] border-black p-3 mt-5">
+                        <div class="size-[160px] mx-auto shrink-0 overflow-hidden rounded-md border border-gray-200">
+                            <img src="/assets/Microservices-icon.png" alt="Website Icon" class="size-full object-cover">
+                        </div>
+
+                        <div class="sm:ml-4 flex flex-1 flex-col">
+                            <div>
+                                <div class="flex justify-between text-base font-medium text-black">
+                                    <div class="text-xl font-bold">
+                                        Virtual Assistant
+                                    </div>
+                                    <a href="https://github.com/joew502/Virtual-Assistant-Alexa" class="flex align-baseline ml-4">
+                                        <svg class="mt-0.3 w-7 h-7 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div class="flex items-baseline space-x-2 mt-1 text-white overflow-x-auto">
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">Go</div>
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">Microservices</div>
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">APIs</div>
+                                </div>
+                                <p class="mt-1 text-base text-black">Developed as part of an Enterprise Computing module, this project was created in accordance to a spec to further our understanding of Microservices. To satisfy the spec I implemented a group of Microservices to create a virtual assistant that can respond to verbal questions. It uses the Microsoft TTS and STT APIs for audio conversion and the Wolfram Alpha Short Answer API for answering the questions.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full block sm:flex justify-center border rounded-md border-[3px] border-black p-3 mt-5">
+                        <div class="size-[160px] mx-auto shrink-0 overflow-hidden rounded-md border border-gray-200">
+                            <img src="/assets/Weather-App-Icon.png" alt="Website Icon" class="size-full object-cover">
+                        </div>
+
+                        <div class="sm:ml-4 flex flex-1 flex-col">
+                            <div>
+                                <div class="flex justify-between text-base font-medium text-black">
+                                    <div class="text-xl font-bold">
+                                        Android Weather App
+                                    </div>
+                                    <a href="https://github.com/joew502/Android-Weather-App" class="flex align-baseline ml-4">
+                                        <svg class="mt-0.3 w-7 h-7 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div class="flex items-baseline space-x-2 mt-1 text-white overflow-x-auto">
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">Android Studio</div>
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">Jave</div>
+                                    <div class="bg-[#ff690D] rounded-xl py-0.5 px-2 max-h-[28px] text-nowrap">Gradle</div>
+                                </div>
+                                <p class="mt-1 text-base text-black">This project is a basic weather app for Android devices, developed as part of a University module. The app utilises the Open Weather Map API to show daily and hourly weather for a chosen location and uses shared preferences to maintain last used location and other features such as multiple activities, menus and implicit and explicit intents.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>

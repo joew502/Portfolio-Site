9fa34e8ab7a99c8e794f70918f2cfcb592d94500
Joe Wilson <joewilson502@gmail.com>
2025-02-27 15:27:54 +0000

    Services added

--- views/services.php
+++ views/services.php
@@ -11,15 +11,45 @@
                 <div class="grid lg:grid-cols-2 grid-cols-1 lg:mt-5 ">
 
                     <div class="max-w-lg mt-2 lg:mr-3 justify-self-center">
-                        <p class="text-balance text-2xl font-semibold tracking-tight text-gray-900 sm:text-3xl">Software Engineer</p>
+                        <p class="text-balance text-2xl font-semibold tracking-tight text-gray-900 sm:text-3xl lg:py-[18px]">Software Engineer</p>
                         <p class="mt-2 ml-1 text-lg/8 text-gray-600">Open to employment, contact and freelance opportunities.</p>
-                        <p class="mt-2 ml-1 text-lg/8 text-gray-600">I am a versatile software engineer with full-stack experience, specialising in Python and Java, with proficiency in Dart (Flutter), Go, JavaScript, TypeScript, and SQL. I have developed web, desktop, and mobile applications, delivering clean, efficient solutions through academic and personal projects, and volunteer contributions to a commercial startup. My studies and use of software development methodologies like Kanban, combined with effective communication skills and experience in version control, allows me to excel in collaborative development environments. I thrive on solving complex problems and ensuring high-quality, timely project delivery, making me a valuable asset to any software team or project.</p>
+                        <ul class="ml-7 sm:ml-10 mt-2 text-lg/8 text-gray-600 list-disc">
+                            <li class="py-0.5">Versatile Full-Stack Software Engineer:
+                                <ul class="ml-7 list-[circle]">
+                                    <li class="py-0.5">Specialises in Python and Java</li>
+                                    <li class="py-0.5">Proficient in Dart (Flutter), Go, JavaScript, TypeScript, and SQL</li>
+                                    <li class="py-0.5">Experience developing web, desktop, and mobile applications</li>
+                                </ul>
+                            </li>
+                            <li class="py-0.5">Proven experience developing clean, efficient code throughout academic projects, personal projects, and volunteer contributions to a commercial startup</li>
+                            <li class="py-0.5">Effective in collaborative development environments:
+                                <ul class="ml-7 list-[circle]">
+                                    <li class="py-0.5">Studied and executed software development methodologies such as Kanban</li>
+                                    <li class="py-0.5">Strong communication skills</li>
+                                    <li class="py-0.5">Version control experience (Git)</li>
+                                </ul>
+                            </li>
+                            <li class="py-0.5">I thrive on solving complex problems and ensuring high-quality, timely project delivery, making me a valuable asset to any software team or project.</li>
+                        </ul>
                     </div>
 
-                    <div class="max-w-lg mt-2 lg:mr-3 justify-self-center">
+                    <div class="max-w-lg mt-8 lg:mt-2 lg:ml-5 justify-self-center">
                         <p class="text-balance text-2xl font-semibold tracking-tight text-gray-900 sm:text-3xl">Technical Operations and Project Management</p>
                         <p class="mt-2 ml-1 text-lg/8 text-gray-600">Open to employment, contact and freelance opportunities.</p>
-                        <p class="mt-2 ml-1 text-lg/8 text-gray-600">I am a solution-oriented operations manager with a passion for problem-solving and optimising technology-driven workflows. I have leveraged my technical skills to successfully manage projects throughout my career. My ability to integrate technology with strategic planning enables smooth operations, efficient resource allocation, and scalable growth. Additionally, I have hands-on experience implementing small-scale infrastructure solutions, including Wi-Fi networks, internal IT systems, video surveillance, and access control. These versatile skills are invaluable in startups and small businesses, where adaptability and logical decision-making are essential.</p>
+                        <ul class="ml-7 sm:ml-10 mt-2 text-lg/8 text-gray-600 list-disc">
+                            <li class="py-0.5">Solution-oriented operations manager with a passion for problem-solving and process optimisation.</li>
+                            <li class="py-0.5">Experienced in project management, driving efficiency and successful execution.</li>
+                            <li class="py-0.5">Skilled in integrating technology with strategic planning to streamline operations, optimise resource allocation, and support business growth.</li>
+                            <li class="py-0.5">Hands-on experience implementing small-scale infrastructure solutions, including:
+                                <ul class="ml-7 list-[circle]">
+                                    <li class="py-0.5">Wi-Fi networks</li>
+                                    <li class="py-0.5">Internal IT systems</li>
+                                    <li class="py-0.5">Video surveillance</li>
+                                    <li class="py-0.5">Access control systems</li>
+                                </ul>
+                            </li>
+                            <li class="py-0.5">Adaptable and logical decision-maker, well-suited for startups and small businesses where versatility is essential.</li>
+                        </ul>
                     </div>
 
                 </div>

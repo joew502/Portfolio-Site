338efe2f3e62be58893d41f875ec743f1ce548a6
Joe Wilson <joewilson502@gmail.com>
2025-03-14 12:00:27 +0000

    Homepage Base Implementation

--- views/homepage.php
+++ views/homepage.php
@@ -1,16 +1,55 @@
 <?php require 'partials/header.php'; ?>
 <div class="min-h-full">
     <?php require 'partials/nav.php'; ?>
-    <header class="mt-16 bg-white shadow">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
-        </div>
-    </header>
-    <main>
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <!-- Your content -->
-            <h1>Hello World</h1>
-        </div>
+    <main class="mt-16">
+    <div class="mx-auto max-w-lg sm:max-w-5xl px-4 py-6 sm:px-6 lg:px-8">
+                <div class="w-full grid grid-cols-1 gap-5 sm:grid-cols-2">
+                    <div class="w-full block sm:flex justify-center border rounded-lg border-gray-300 shadow-lg p-3 shadow-gray-300 bg-white">
+                        <div class="mt-2 sm:mt-0 sm:ml-4 flex flex-1 flex-col">
+                            <div class="flex justify-between text-base font-medium text-black">
+                                <div class="text-xl font-bold">
+                                    Personal Website
+                                </div>
+                            </div>
+                            <p class="mt-1 text-base text-black">A personal project to display my skills, projects and services.</p>
+                        </div>
+                    </div>
+                    
+                    <div class="w-full block sm:flex justify-center border rounded-lg border-gray-300 shadow-lg p-3 shadow-gray-300 bg-white">
+                        <div class="mt-2 sm:mt-0 sm:ml-4 flex flex-1 flex-col">
+                            <div class="flex justify-between text-base font-medium text-black">
+                                <div class="text-xl font-bold">
+                                    Personal Website
+                                </div>
+                            </div>
+                            <p class="mt-1 text-base text-black">A personal project to display my skills, projects and services.</p>
+                        </div>
+                    </div>
+
+                    <div class="w-full block sm:flex justify-center border rounded-lg border-gray-300 shadow-lg p-3 shadow-gray-300 bg-white">
+                        <div class="mt-2 sm:mt-0 sm:ml-4 flex flex-1 flex-col">
+                            <div class="flex justify-between text-base font-medium text-black">
+                                <div class="text-xl font-bold">
+                                    Personal Website
+                                </div>
+                            </div>
+                            <p class="mt-1 text-base text-black">A personal project to display my skills, projects and services.</p>
+                        </div>
+                    </div>
+
+                    <div class="w-full block sm:flex justify-center border rounded-lg border-gray-300 shadow-lg p-3 shadow-gray-300 bg-white">
+                        <div class="mt-2 sm:mt-0 sm:ml-4 flex flex-1 flex-col">
+                            <div class="flex justify-between text-base font-medium text-black">
+                                <div class="text-xl font-bold">
+                                    Personal Website
+                                </div>
+                            </div>
+                            <p class="mt-1 text-base text-black">A personal project to display my skills, projects and services.</p>
+                        </div>
+                    </div>
+        
+                </div>
+            </div>
     </main>
 </div>
 <?php require 'partials/footer.php'; ?>
